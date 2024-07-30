8818d07199a88adeb841885f9b4191a6d931a970
om <omchik86@mail.ru>
2024-07-30 12:23:00 +0300

    20 july Order work
    Signed-off-by: om <omchik86@mail.ru>

--- orders/forms.py
+++ orders/forms.py
@@ -1,34 +1,31 @@
 import re
 from django import forms
 
-
 class CreateOrderForm(forms.Form):
     first_name = forms.CharField()
     last_name = forms.CharField()
     phone_number = forms.CharField()
     requires_delivery = forms.ChoiceField(
         choices=[
-            ("0", False),
-            ("1", True),
+            ("0", "Нет"),
+            ("1", "Да"),
         ],
     )
     delivery_address = forms.CharField(required=False)
     payment_on_get = forms.ChoiceField(
         choices=[
-            ("0", 'False'),
-            ("1", 'True'),
+            ("0", "Нет"),
+            ("1", "Да"),
         ],
     )
 
     def clean_phone_number(self):
         data = self.cleaned_data['phone_number']
 
-        if not data.isdigit():
-            raise forms.ValidationError("Номер телефона должен содержать только цифры")
-
-        pattern = re.compile(r'^\d{10}₽')
+        # Убедитесь, что номер телефона состоит только из цифр и имеет длину 10 символов
+        pattern = re.compile(r'^\d{10}$')
         if not pattern.match(data):
-            raise forms.ValidationError("Неверный формат номера")
+            raise forms.ValidationError("Неверный формат номера. Должно быть 10 цифр.")
 
         return data
 
--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from django.db import transaction
@@ -8,10 +7,8 @@ from django.urls import reverse_lazy
 from django.views.generic import FormView
 
 from carts.models import Cart
-
 from orders.forms import CreateOrderForm
 from orders.models import Order, OrderItem
-
 
 class CreateOrderView(LoginRequiredMixin, FormView):
     template_name = 'orders/create_order.html'
@@ -47,8 +44,7 @@ class CreateOrderView(LoginRequiredMixin, FormView):
                         quantity = cart_item.quantity
 
                         if product.quantity < quantity:
-                            raise ValidationError(f'Недостаточное количество товара {name} на складе\
-                                                       В наличии - {product.quantity}')
+                            raise ValidationError(f'Недостаточное количество товара {name} на складе. В наличии - {product.quantity}')
 
                         OrderItem.objects.create(
                             order=order,
@@ -64,9 +60,9 @@ class CreateOrderView(LoginRequiredMixin, FormView):
                     cart_items.delete()
 
                     messages.success(self.request, 'Заказ оформлен!')
-                    return redirect('user:profile')
+                    return redirect('users:profile')
         except ValidationError as e:
-            messages.success(self.request, str(e))
+            messages.error(self.request, str(e))
             return redirect('orders:create_order')
 
     def form_invalid(self, form):
@@ -78,67 +74,3 @@ class CreateOrderView(LoginRequiredMixin, FormView):
         context['title'] = 'Оформление заказа'
         context['order'] = True
         return context
-
-# @login_required
-# def create_order(request):
-#     if request.method == 'POST':
-#         form = CreateOrderForm(data=request.POST)
-#         if form.is_valid():
-#             try:
-#                 with transaction.atomic():
-#                     user = request.user
-#                     cart_items = Cart.objects.filter(user=user)
-
-#                     if cart_items.exists():
-#                         # Создать заказ
-#                         order = Order.objects.create(
-#                             user=user,
-#                             phone_number=form.cleaned_data['phone_number'],
-#                             requires_delivery=form.cleaned_data['requires_delivery'],
-#                             delivery_address=form.cleaned_data['delivery_address'],
-#                             payment_on_get=form.cleaned_data['payment_on_get'],
-#                         )
-#                         # Создать заказанные товары
-#                         for cart_item in cart_items:
-#                             product=cart_item.product
-#                             name=cart_item.product.name
-#                             price=cart_item.product.sell_price()
-#                             quantity=cart_item.quantity
-
-
-#                             if product.quantity < quantity:
-#                                 raise ValidationError(f'Недостаточное количество товара {name} на складе\
-#                                                        В наличии - {product.quantity}')
-
-#                             OrderItem.objects.create(
-#                                 order=order,
-#                                 product=product,
-#                                 name=name,
-#                                 price=price,
-#                                 quantity=quantity,
-#                             )
-#                             product.quantity -= quantity
-#                             product.save()
-
-#                         # Очистить корзину пользователя после создания заказа
-#                         cart_items.delete()
-
-#                         messages.success(request, 'Заказ оформлен!')
-#                         return redirect('user:profile')
-#             except ValidationError as e:
-#                 messages.success(request, str(e))
-#                 return redirect('orders:create_order')
-#     else:
-#         initial = {
-#             'first_name': request.user.first_name,
-#             'last_name': request.user.last_name,
-#             }
-
-#         form = CreateOrderForm(initial=initial)
-
-#     context = {
-#         'title': 'Home - Оформление заказа',
-#         'form': form,
-#         'order': True,
-#     }
-#     return render(request, 'orders/create_order.html', context=context)
